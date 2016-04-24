c0530f6984c7e3101ba833087c5b8d14ffa062e0
Uluc Sengil <usengil@ku.edu.tr>
2016-04-24 18:59:14 +0300

    nonempty now?

--- textbox.rb
+++ textbox.rb
@@ -28,6 +28,7 @@ class Textbox < Formula
     # system "cmake", ".", *std_cmake_args
     # system "make", "install" # if this fails, try separate make/make install steps
     system "./install.sh"
+    prefix.install Dir["*"]
   end
 
   test do
